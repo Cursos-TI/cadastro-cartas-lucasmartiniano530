df19bdde6bbb1d8379c37a9f786b0b78e1a8aed6
lucasmartiniano530 <lucasmartiniano08@gmail.com>
2025-10-08 01:38:07 +0000

    novas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -15,7 +15,19 @@ int main() {
   float pib = 3000000;
   int pontos_turisticos = 15;
 
+  /* CARTA 2*/
+
+  char estado2[3] = "A";
+  char Codigo_da_carta2[20] = "A02";
+  char nome2[20] = "Goias";
+  int populacao2 = 250000;
+  float area2 = 15000000;
+  float pib2 = 2000000;
+  int pontos_turisticos2 = 5;
+
+
   printf("Carta SuperTrunfo\n");
+  printf("CARTA 01\n");
   printf("Estado: %s\n", estado);
   printf("Codigo da carta: %s\n", Codigo_da_carta);
   printf("Nome da cidade: %s\n", nome);
@@ -24,5 +36,16 @@ int main() {
   printf("PIB: %f\n", pib);
   printf("Quantidade pontos turisticos: %d\n", pontos_turisticos);
   
+
+  printf("\nCARTA 02\n");
+  printf("Estado: %s\n", estado2);
+  printf("Codigo da carta: %s\n", Codigo_da_carta2);
+  printf("Nome da cidade: %s\n", nome2);
+  printf("Populacao total: %d\n", populacao2);
+  printf("Area total: %f\n", area2);
+  printf("PIB: %f\n", pib2);
+  printf("Quantidade pontos turisticos: %d\n", pontos_turisticos2);
+
+
 return 0;
 } 
